1d8edcb5788ba5e5309bfa003bdc8918ed14d0d8
Shafique Jamal <shafique.jamal@gmail.com>
2015-08-20 23:27:46 -0400

    Add subject data

--- run_analysis.R
+++ run_analysis.R
@@ -13,12 +13,14 @@ variable_names <- as.character(features[,2])
 # Assemble test data set
 test_data_without_activities <- read.table('X_test.txt', col.names = variable.names)
 test_activities <- read.table('Y_test.txt', col.names = c("activity"))
-test_data <- bind_cols(test_activities, test_data_without_activities)
+test_subjects <- read.table('subject_test.txt', col.names = c("subject"))
+test_data <- bind_cols(test_subjects, test_activities, test_data_without_activities)
 
 # Assemble training data set
 train_data_without_activities <- read.table('X_train.txt', col.names = variable.names)
 train_activities <- read.table('Y_train.txt', col.names = c("activity"))
-train_data <- bind_cols(train_activities, train_data_without_activities)
+train_subjects <- read.table('subject_train.txt', col.names = c("subject"))
+train_data <- bind_cols(train_subjects, train_activities, train_data_without_activities)
 
 # Join testing and training set
 full_data <- bind_rows(test_data, train_data)
@@ -30,7 +32,7 @@ full_data <- bind_rows(test_data, train_data)
 #
 ####################################################################################
 
-means_sds <- select(full_data, activity, contains("mean"), contains("std"))
+means_sds <- select(full_data, activity, subject, contains("mean"), contains("std"))
 
 ####################################################################################
 #
@@ -44,7 +46,6 @@ activities <- read.table('activity_labels.txt')
 names(activities) <- c("activity_number", "activity_name")
 names(means_sds)[names(means_sds)=="activity"] <- "activity_number"
 merged <- merge(means_sds, activities, 'activity_number', 'activity_number')
-names(merged)[is.na(names(merged))] <- "activity_name"
 
 ####################################################################################
 #
@@ -65,4 +66,5 @@ names(merged) <- sub("^t", "time", names(means_sds))
 names(merged) <- sub("^f", "freq", names(means_sds))
 names(merged) <- sub("BodyBody", "Body", names(means_sds))
 names(merged) <- sub("angle.tBody", "angle.timeBody", names(means_sds))
+names(merged)[is.na(names(merged))] <- "activity_name"
 
