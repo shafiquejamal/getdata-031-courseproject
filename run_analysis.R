e299b143d984f577ae5ccf2d419e627c3e23e8e6
Shafique Jamal <shafique.jamal@gmail.com>
2015-08-20 21:24:27 -0400

    For each of the test and train datasets, combine the id data with the measurement data, including the variable names in the dataframes. Then stack the two datasets to produce the full dataset

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,38 @@
+library(dplyr)
+
+####################################################################################
+#
+# Part 1: Merges the training and the test sets to create one data set.
+#
+####################################################################################
+
+# Assemble test data set
+features <- read.table('features.txt')
+variable_names <- as.character(features[,2])
+
+test_data_without_ids <- read.table('X_test.txt', col.names = variable.names)
+test_ids <- read.table('Y_test.txt', col.names = c("id"))
+test_data <- bind_cols(test_ids, test_data_without_ids)
+dim(test_data_without_ids)
+dim(test_ids)
+dim(test_data)
+
+# Assemble training data set
+train_data_without_ids <- read.table('X_train.txt', col.names = variable.names)
+train_ids <- read.table('Y_train.txt', col.names = c("id"))
+train_data <- bind_cols(train_ids, train_data_without_ids)
+dim(train_data_without_ids)
+dim(train_ids)
+dim(train_data)
+
+# Join testing and training set
+full_data <- bind_rows(test_data, train_data)
+dim(full_data)
+
+####################################################################################
+#
+# Part 2: Extracts only the measurements on the mean and standard deviation for each 
+#           measurement. 
+#
+####################################################################################
 
