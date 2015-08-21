bf13c1f99bb137e4d59f8a2d4f6eb9ab9a2fe5a0
Shafique Jamal <shafique.jamal@gmail.com>
2015-08-20 21:33:06 -0400

    Extract only the measurements on the mean and standard deviation of each measurement.

--- run_analysis.R
+++ run_analysis.R
@@ -6,28 +6,22 @@ library(dplyr)
 #
 ####################################################################################
 
-# Assemble test data set
+# Load the variable names
 features <- read.table('features.txt')
 variable_names <- as.character(features[,2])
 
+# Assemble test data set
 test_data_without_ids <- read.table('X_test.txt', col.names = variable.names)
 test_ids <- read.table('Y_test.txt', col.names = c("id"))
 test_data <- bind_cols(test_ids, test_data_without_ids)
-dim(test_data_without_ids)
-dim(test_ids)
-dim(test_data)
 
 # Assemble training data set
 train_data_without_ids <- read.table('X_train.txt', col.names = variable.names)
 train_ids <- read.table('Y_train.txt', col.names = c("id"))
 train_data <- bind_cols(train_ids, train_data_without_ids)
-dim(train_data_without_ids)
-dim(train_ids)
-dim(train_data)
 
 # Join testing and training set
 full_data <- bind_rows(test_data, train_data)
-dim(full_data)
 
 ####################################################################################
 #
@@ -35,4 +29,6 @@ dim(full_data)
 #           measurement. 
 #
 ####################################################################################
+
+means_sds <- select(full_data, id, contains("mean"), contains("std"))
 
