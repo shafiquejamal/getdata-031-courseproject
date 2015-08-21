50473c84e5aa82c32ebb107791850837a0b6f6d2
Shafique Jamal <shafique.jamal@gmail.com>
2015-08-21 00:04:31 -0400

    Fix some errors in copying and pasting variable names.

--- run_analysis.R
+++ run_analysis.R
@@ -15,12 +15,14 @@ test_data_without_activities <- read.table('X_test.txt', col.names = variable.na
 test_activities <- read.table('Y_test.txt', col.names = c("activity"))
 test_subjects <- read.table('subject_test.txt', col.names = c("subject"))
 test_data <- bind_cols(test_subjects, test_activities, test_data_without_activities)
+test_data <- mutate(test_data, test.or.train="test")
 
 # Assemble training data set
 train_data_without_activities <- read.table('X_train.txt', col.names = variable.names)
 train_activities <- read.table('Y_train.txt', col.names = c("activity"))
 train_subjects <- read.table('subject_train.txt', col.names = c("subject"))
 train_data <- bind_cols(train_subjects, train_activities, train_data_without_activities)
+train_data <- mutate(train_data, test.or.train="train")
 
 # Join testing and training set
 full_data <- bind_rows(test_data, train_data)
@@ -46,6 +48,7 @@ activities <- read.table('activity_labels.txt')
 names(activities) <- c("activity_number", "activity_name")
 names(means_sds)[names(means_sds)=="activity"] <- "activity_number"
 merged <- merge(means_sds, activities, 'activity_number', 'activity_number')
+merged <- select(merged, -activity_number)
 
 ####################################################################################
 #
@@ -56,15 +59,24 @@ merged <- merge(means_sds, activities, 'activity_number', 'activity_number')
 # Done - I did this while reading in the data.
 # I think the variable names are fine as is. But, I will change Acc to Acceleration,
 #   Gyro to Gyroscope, and get rid of double-dots
-names(merged) <- sub("Gyro", "Gyroscope", names(means_sds))
-names(merged) <- sub("Acc", "Acceleration", names(means_sds))
-names(merged) <- sub("\\.\\.\\.", ".", names(means_sds))
-names(merged) <- sub("\\.\\.$", "", names(means_sds))
-names(merged) <- sub("\\.\\.", ".", names(means_sds))
-names(merged) <- sub("\\.$", "", names(means_sds))
-names(merged) <- sub("^t", "time", names(means_sds))
-names(merged) <- sub("^f", "freq", names(means_sds))
-names(merged) <- sub("BodyBody", "Body", names(means_sds))
-names(merged) <- sub("angle.tBody", "angle.timeBody", names(means_sds))
+names(merged) <- sub("Gyro", "Gyroscope", names(merged))
+names(merged) <- sub("Acc", "Acceleration", names(merged))
+names(merged) <- sub("\\.\\.\\.", ".", names(merged))
+names(merged) <- sub("\\.\\.$", "", names(merged))
+names(merged) <- sub("\\.\\.", ".", names(merged))
+names(merged) <- sub("\\.$", "", names(merged))
+names(merged) <- sub("^t", "time", names(merged))
+names(merged) <- sub("^f", "freq", names(merged))
+names(merged) <- sub("BodyBody", "Body", names(merged))
+names(merged) <- sub("angle.tBody", "angle.timeBody", names(merged))
 names(merged)[is.na(names(merged))] <- "activity_name"
 
+####################################################################################
+#
+# Part 5: From the data set in step 4, creates a second, independent tidy data set 
+#   with the average of each variable for each activity and each subject.
+#       
+####################################################################################
+
+tidy <- merged %>% group_by(subject, activity_name) %>% summarise_each(funs(mean)) 
+write.table(tidy, "tidy.txt", row.names = F)
